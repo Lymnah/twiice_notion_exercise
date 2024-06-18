bb68c63c9ced1010ca53833c49d8bd63519b9d71
Charly <charly@mancel.net>
2024-06-18 12:50:51 +0200

    deps: add Boost library

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include "boost_example.h"
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    testBoostLib();
+
     w.show();
     return a.exec();
 }
